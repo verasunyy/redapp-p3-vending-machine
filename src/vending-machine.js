607398c317d8a9e1f5be6d69d071e057b4c44fdd
verasunyy <verasun@live.com>
2019-08-18 22:52:32 -0700

    add dispense change method

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -10,7 +10,11 @@ class VendingMachine {
   getProductInventoryByPosition(position) {
     return this.inventory.products[position].inventory
   }
+  getProductPriceByPosition(position) {
+    return this.inventory.products[position].price
+  }
   getChangeInventory() {
+    console.log(this.inventory.changes)
     return this.inventory.changes
   }
   getChangeInventoryByName(name) {
@@ -46,12 +50,67 @@ class VendingMachine {
     }
   }
 
+  dispenseProduct(position, amount) {
+    if (amount > 20) {
+      throw new Error("Not accept bill larger than 20");
+    } else if (amount < this.getProductPriceByPosition(position)) {
+      throw new Error("Not Enough Balance");
+    } else if (!this.getProductInventoryByPosition(position)) {
+      throw new Error("Product Out of Stock");
+    } else {
+      let change = {}
+      let changeAmount = amount - this.getProductPriceByPosition(position)
+      this.setProductInventory(position, -1);
+      return changeAmount;
 
 
+    }
+  }
 
+  dispenseChanges(changeAmount) {
+    let changes = [
+      { "name": "tens", "number": 0, "value": 10 },
+      { "name": "fives", "number": 0, "value": 5 },
+      { "name": "toonies", "number": 0, "value": 2 },
+      { "name": "loonies", "number": 0, "value": 1 },
+      { "name": "quaters", "number": 0, "value": 0.25 },
+      { "name": "dimes", "number": 0, "value": 0.10 },
+      { "name": "nicles", "number": 0, "value": 0.05 }
+    ]
+    this.checkChange(changes, changeAmount);
+    console.log("changes: \n", changes)
+    changes.map((change, index) =>
+      this.setChangeInventory(change.name, -change.number)
+    )
+    return changes.reduce((acc, current) => (acc = acc + current.number * current.value), 0);
+  }
+
+  checkChange(array, amount) {
+    array.map((change, index) => {
+      if (amount >= change.value) {
+        const numberOfChange = Math.floor(amount / change.value);
+        if (this.getChangeInventoryByName(change.name) >= numberOfChange) {
+          array[index].number = numberOfChange;
+          amount = parseFloat((amount - change.value * numberOfChange).toFixed(2));
+        } else {
+          array[index].number = this.getChangeInventoryByName(change.name);
+          amount = amount - change.value * this.getChangeInventoryByName(change.name)
+          if (change.name === "nicles") {
+            if (this.getChangeInventoryByName("dimes") >= array[5].number + 3) {
+              array[4].number--;
+              array[5].number += 3;
+              amount = amount - 0.05;
+            } else {
+              throw new Error("Sorry, not enough changes to give back, please try again")
+            }
+
+          }
+        }
+      }
+    })
+    if (amount !== 0) {
+      throw new Error("Sorry, not enough changes to give back, please try again")
+    }
+  }
 }
 module.exports = VendingMachine;
-const vw = new VendingMachine(inventory);
-// console.log(vw.getInventory("A1"));
-// console.log(vw.setProductInventory("A1", 6));
-// console.log(vw.setChangeInventory("toonies", 25));
